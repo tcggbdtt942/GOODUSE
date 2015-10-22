e7e7af3ac62f2d8df70060216887960b598a8e4d
陈玥 <523242289@qq.com>
2015-10-22 16:49:18 +0800

    no message

---
+++ 未命名文件夹/iOSinClass/iOSinClass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSinClass
+//
+//  Created by olch on 15/10/22.
+//  Copyright (c) 2015年 olch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 未命名文件夹/iOSinClass/iOSinClass/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSinClass
+//
+//  Created by olch on 15/10/22.
+//  Copyright (c) 2015年 olch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
