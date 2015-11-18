29a713b1bb91d090aacf259ede365b014c45fb02
chenyue <523242289@qq.com>
2015-11-18 15:44:06 +0800

    dfbsd

---
+++ iOSinClass/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.h
+//  iOSinClass
+//
+//  Created by DMT on 15/11/18.
+//  Copyright (c) 2015年 olch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
